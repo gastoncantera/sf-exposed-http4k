719662ad0fb13e434ddb8a9ec3da894fa6a7c204
Gaston Cantera <65862015+gastoncantera@users.noreply.github.com>
2023-11-10 16:13:54 -0300

    hikari connection pool

--- src/main/kotlin/modules/DatabaseProvider.kt
+++ src/main/kotlin/modules/DatabaseProvider.kt
@@ -2,6 +2,8 @@
 
 package modules
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import domain.UserEntity
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
@@ -9,21 +11,32 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseProvider {
 
-    val readOnlyDb = Database.connect(
-        "jdbc:postgresql://localhost:5433/testdb",
-        driver = "org.postgresql.Driver",
-        user = "postgres",
-        password = "postgres"
+    private val readOnlyDataSource = HikariDataSource(
+        HikariConfig().apply {
+            jdbcUrl = "jdbc:postgresql://localhost:5433/testdb"
+            driverClassName = "org.postgresql.Driver"
+            username = "postgres"
+            password = "postgres"
+        }
     )
+    val readOnlyDb = Database.connect(readOnlyDataSource)
 
     fun initialize() {
-        // latest connection is the default exposed connection
-        Database.connect(
-            "jdbc:postgresql://localhost:5432/testdb",
-            driver = "org.postgresql.Driver",
-            user = "postgres",
-            password = "postgres"
+        val readWriteDataSource = HikariDataSource(
+            HikariConfig().apply {
+                jdbcUrl = "jdbc:postgresql://localhost:5432/testdb"
+                driverClassName = "org.postgresql.Driver"
+                username = "postgres"
+                password = "postgres"
+            }
         )
+
+        /*
+        * Make sure this connection is established AFTER the read only connection.
+        * Latest connection is the default connection in Jetbrains Exposed.
+         */
+        Database.connect(readWriteDataSource)
+
         schemaInitialize()
     }
 
