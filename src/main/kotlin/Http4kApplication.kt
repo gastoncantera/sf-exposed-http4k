2e1d15bd7548b831b590cb9ffbeb68ce757444d9
Gaston Cantera <65862015+gastoncantera@users.noreply.github.com>
2023-11-13 13:20:06 -0300

    using netty

--- src/main/kotlin/Http4kApplication.kt
+++ src/main/kotlin/Http4kApplication.kt
@@ -2,7 +2,7 @@ import controller.UserController
 import org.http4k.core.HttpHandler
 import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters.PrintRequest
-import org.http4k.server.SunHttp
+import org.http4k.server.Netty
 import org.http4k.server.asServer
 import modules.DatabaseProvider
 
@@ -11,7 +11,9 @@ fun main() {
 
     val printingApp: HttpHandler = PrintRequest().then(UserController.app)
 
-    val server = printingApp.asServer(SunHttp(8080)).start()
+    val server = printingApp.asServer(Netty(8080)).start()
 
     println("Server started on " + server.port())
+
+    server.block()
 }
